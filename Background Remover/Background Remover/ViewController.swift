33c2ba1900930240bc19c228307d09f44c3e49b6
Husnain Ali <ali@ili.digital>
2021-11-11 11:10:14 +0500

    Updated code
    Updated code

---
+++ Background Remover/Background Remover/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Background Remover
+//
+//  Created by devadmin on 11/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
